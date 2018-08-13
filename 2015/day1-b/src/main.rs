26807614869bab98780098a71f6431df27e0b3d7
Brooks McKinley <brooksmckinley@yahoo.com>
2018-08-13 19:54:24 -0400

    Completed problem 1B

--- 2015/day1-b/src/main.rs
+++ 2015/day1-b/src/main.rs
@@ -10,13 +10,19 @@ fn get_line() -> String {
 fn main() {
     let input = get_line();
     let mut floor: i32 = 0;
+    let mut position = 0;
     for letter in input.as_str().chars() {
+        position += 1;
         if letter == '(' {
             floor = floor + 1;
         }
         else if letter == ')' {
             floor = floor - 1;
         }
+        // Check if Santa has reached the basement yet
+        if floor == -1 {
+            break;
+        }
     }
-    println!("Floor: {}", floor);
+    println!("Position where Santa first entered the basement: {}", position);
 }
