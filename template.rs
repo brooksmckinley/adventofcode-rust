1792f46e5458066a98ce43a6705caa241c798ba9
Brooks McKinley <brooksmckinley@yahoo.com>
2018-08-13 20:09:43 -0400

    Unwrap result from get_line()

--- template.rs
+++ template.rs
@@ -3,7 +3,7 @@ use std::io;
 fn get_line() -> String {
     let mut input = String::new();
     let stdin = io::stdin();
-    stdin.read_line(&mut input);
+    stdin.read_line(&mut input).unwrap();
     input
 }
 fn main() {
