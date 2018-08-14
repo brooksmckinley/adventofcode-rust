2fbc45fad5e197006db8e0fcfa21d8c901f7d52e
Brooks McKinley <brooksmckinley@yahoo.com>
2018-08-13 20:19:48 -0400

    Started (and completed) puzzle 2B

---
+++ 2015/day2-b/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+fn get_line() -> String {
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input).unwrap();
+    input
+}
+fn main() {
+    let mut total = 0;
+    let mut total_ribbon = 0;
+    // Loop until the line doesn't include enough numbers for the calculation
+    loop {
+        let line = get_line();
+        let line = line.trim();
+        // Collect all of the numbers into a Vec
+        let nums: Vec<&str> = line.split("x").collect();
+        // If the Vec isn't long enough, break the loop
+        if nums.len() < 3 {
+            break;
+        }
+        let length: i32 = nums[0].parse().unwrap();
+        let width: i32 = nums[1].parse().unwrap();
+        let height: i32 = nums[2].parse().unwrap();
+        let surface_area = (2 * length * width) + (2 * width * height) + (2 * height * length);
+        total += surface_area;
+
+        // Calculate slack
+        let mut sides = vec![length, width, height];
+        sides.sort();
+        let slack = sides[0] * sides[1];
+        total += slack;
+
+        // Calculate ribbon
+        let ribbon = (2 * sides[0]) + (2 * sides[1]);
+        let bow = length * width * height;
+        total_ribbon += ribbon + bow;
+    }
+    println!("Total surface area: {}", total);
+    println!("Total ribbon required: {}", total_ribbon);
+}
